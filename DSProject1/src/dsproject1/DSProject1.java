ec9e25589bb7306c3c4e293604b9461265e95827
Bahaa <219110085@psu.edu.sa>
2021-09-29 23:47:39 +0300

    IDE Change

--- DSProject1/src/dsproject1/DSProject1.java
+++ DSProject1/src/dsproject1/DSProject1.java
@@ -3,6 +3,7 @@ package dsproject1;
 public class DSProject1 {
     public static void main(String[] args) {
         System.out.println("Bahaa First Commit, and maybe last");
+        System.out.println("Switching IDE Test Pull");
     }
     
 }
