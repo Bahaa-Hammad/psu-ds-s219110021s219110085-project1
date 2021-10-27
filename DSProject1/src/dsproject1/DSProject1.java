75efa6c9a12e43e67ed6b676d07d8e73316099d0
abdulrhmanessa <AbdulrhmanAissa@hotmail.com>
2021-10-28 00:26:18 +0300

    The final code with the updated main class for the project discussion.
    Version 1.6

--- DSProject1/src/dsproject1/DSProject1.java
+++ DSProject1/src/dsproject1/DSProject1.java
@@ -2,21 +2,28 @@
 package dsproject1;
 public class DSProject1 {
     public static void main(String[] args) {
-        BigInt x = new BigInt("111");
-        BigInt y = new BigInt("999");
-        BigInt z = x.subtract(y);
-        z.representation.printList();
-        //System.out.println(z.toString());
-        System.out.println(z.toString());
-        //System.out.println(y.toString());
-        //x.representation.printList();
-        //y.representation.printList();
-        //BigInt z = x.subtract(y);
-        //z.representation.printList();
-        //System.out.println(z.toString());
-        //System.out.println(x.representation.size);
-        //System.out.println(x.toString());
-        //x.representation.printList();
-        //System.out.println(x.compare(y));
+        // Input x
+        BigInt x = new BigInt("1111111");
+        System.out.println("The first number in DLL representation: ");
+        x.representation.printList();
+        System.out.println("The first number in toString: " + x);
+        System.out.println();
+
+        // Input y
+        BigInt y = new BigInt("9999999");
+        System.out.println("The second number in DLL representation: ");
+        y.representation.printList();
+        System.out.println("The second number in toString: " + y);
+        System.out.println();
+
+        // Methods
+        System.out.println("The result of addition: ");
+        BigInt a = x.add(y);
+        System.out.println(a);
+        System.out.println("The result of subtraction: ");
+        BigInt s = x.subtract(y);
+        System.out.println(s);
+        System.out.println("The result of comparing: ");
+        System.out.println(+ x.compare(y));
     }
 }
